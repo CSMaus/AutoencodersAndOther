b8990bcfac241f638802a6898cac129533670939
Kseniia Barashok <cs-maus-1@mail.ru>
2022-06-16 15:15:02 +0900

    now can SplitDataset to split data into train, validation and test and to delete it

--- SplitDataset.py
+++ SplitDataset.py
@@ -1,4 +1,61 @@
 # this script i'm gonna use to split dataset into training, validation and test
+import parameters as p, numpy as np, sys, os
+from tqdm import tqdm
+import shutil
+
+
+# here I'm just copying files
+# it's possible to replace files from "datas_folder" to "destin_folder"
+# but to the moment I need just copy files, so it will not be random
+
+datas_folder = p.dataset_ToSplit_folder
+destin_folder = f'{p.destin_splitted_folder}'
+v_s = p.validation_split
+t_s = p.test_split
+
+path = os.path.join(datas_folder)
+path_list = os.listdir(path)
+print('Number of categories', np.shape(path_list)[0])
+print('Categories is:', path_list)
+
+if p.clear_splitted_data:
+    if os.path.exists(destin_folder):
+        tqdm(shutil.rmtree(destin_folder, ignore_errors=True))
+    print('Data deleted')
+
+if p.split_data:
+    for category in path_list:
+        imgs = os.listdir(os.path.join(datas_folder, category))
+        num_imgs = len(imgs)
+        print(f'\nTotal number of images in category {category}:', num_imgs)
+
+        i = 0
+        for img in tqdm(imgs):
+
+            if i < int(num_imgs * (1 - v_s - t_s)):
+                train_val_folder = 'train'
+            elif int(num_imgs * (1 - v_s - t_s)) <= i < int(num_imgs * (1 - t_s)):
+                train_val_folder = 'validation'
+            else:
+                train_val_folder = 'test'
+            i += 1
+            destin = f'{destin_folder}/{train_val_folder}/{category}'
+            if os.path.exists(destin):
+                while not os.path.exists(f'{destin}/{img}'):
+                    shutil.copyfile(f'{datas_folder}/{category}/{img}', f'{destin}/{img}')
+            else:
+                os.makedirs(destin)
+                while not os.path.exists(f'{destin}/{img}'):
+                    shutil.copyfile(f'{datas_folder}/{category}/{img}', f'{destin}/{img}')
+            # break
+
+
+
+
+
+
+
+
 
 
 
--- parameters.py
+++ parameters.py
@@ -1,15 +1,31 @@
 using_dot_generator = False
+
+
+# ################## datas parameters (img size, color, etc)  ######################
 n = 8
 img_size = 224
-mnist_d = False
-
-use_flow_from_directory = True
-use_flow = True
-dataset_folder = 'D:/DataSets/dogs_cats/'
-
 img_as_gray = False
 
-# now batch size depends on number of image data
+
+# ############### data settings: type of data, folder, library, etc ###############
+dataset_folder = 'D:/DataSets/dogs_cats'
+dataset_ToSplit_folder = 'D:/DataSets/dogs_cats'
+destin_splitted_folder = 'D:/DataSets/splitted/dogs_cats'
+
+# #################### if test_split = 0, then will be only validation and test ##################
+# if u need split data on 3 categories, then set non zero values here
+# but if u already split the data on 2 categories and want to resplit it into 3, u should delete earlier splitted data
+clear_splitted_data = False
+split_data = True
+validation_split = 0.2
+test_split = 0.1
+
+mnist_d = False
+use_flow_from_directory = True
+use_flow = True
+
+
+# ##################### training parameters for models ############################
 batch_size = 100
 latent_dim = 256  # to be easier generate and visualize result
 dropout_r = 0.3
